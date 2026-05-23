ac3a1f92fabd7ee802ee94466139f4092b05b832
George Legun <georgelegun@gmail.com>
2026-05-23 20:16:40 +0300

    add connection details

--- docker_setup/start_docker.sh
+++ docker_setup/start_docker.sh
@@ -4,3 +4,16 @@
 docker run -d --name oracle-db-proiect -p 1522:1521 \
    --restart unless-stopped -e ORACLE_PASSWORD=parola123 \
    gvenzl/oracle-xe
+
+# Conexiune 1:
+#   ADDR: 10.19.49.10:1522 
+#   USER: "SYS"
+#   PAROLA: "parola123"
+#   Service Name: "XE"
+# 
+# Conexiune 2:
+#   ADDR: 10.19.49.10:1522 
+#   USER: "proiect"
+#   PAROLA: "parola123"
+#   Service Name: "XE"
+# 
